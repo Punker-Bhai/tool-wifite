b9efbcdf074bd1b474ee5739c0513d5f1d9f53fa
HACKERSPUNK <79210553+Punker-Bhai@users.noreply.github.com>
2021-02-25 11:32:55 +0530

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,44 @@
+clear
+
+sudo chmod +x /data/data/com.termux/files/usr/etc
+
+clear
+
+sudo chmod +x /data/data/com.termux/files/usr/share/doc/
+
+clear
+
+sudo rm -rf /data/data/com.termux/files/usr/share/doc/tool-wifite
+
+clear
+
+cd /etc/
+
+clear
+
+sudo rm -rf /etc/Punker-Bhai
+
+clear
+
+mkdir Punker-Bhai
+
+clear
+
+cd MKDIRLOVE
+
+clear
+
+git clone https://github.com/Punker-Bhai/tool-wifite
+clear
+
+cd Punker-Bhai
+
+clear
+
+sudo chmod +x install.sh
+
+clear
+
+./install.sh
+
+clear
